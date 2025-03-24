c768fe551d1e5ce70bd8f0847af54ae58d688e33
Lucas Fajardo <84720413+SilvergamesBR@users.noreply.github.com>
2025-03-24 15:35:14 -0300

    Adds looping to the carousel

--- scripts.js
+++ scripts.js
@@ -56,11 +56,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (leftArrow && rightArrow && slider) {
     leftArrow.addEventListener('click', function () {
-      slider.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+      if (slider.scrollRight + slider.clientWidth >= slider.scrollWidth - 1) {
+        slider.scrollTo({ right: 0, behavior: 'smooth' });
+      } else {
+        slider.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+      }
     });
 
     rightArrow.addEventListener('click', function () {
-      slider.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+      if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth - 1) {
+        slider.scrollTo({ left: 0, behavior: 'smooth' });
+      } else {
+        slider.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+      }
     });
   }
 
