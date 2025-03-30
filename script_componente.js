603df784a3b077ceaf8fa1033ce420fd971b35ef
Lucas Fajardo <84720413+SilvergamesBR@users.noreply.github.com>
2025-03-30 20:58:48 -0300

    Adds colour change based on nota

--- script_componente.js
+++ script_componente.js
@@ -1,51 +1,60 @@
 class AulasComponent extends HTMLElement {
     constructor() {
-      super();
-      this.attachShadow({ mode: 'open' });
-      this.hoje = "ter";
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.hoje = "ter";
     }
-  
+
     connectedCallback() {
-      this.loadData();
+        this.loadData();
     }
-  
+
     async loadData() {
-      try {
-        const response = await fetch('aulas.json');
-        const aulas = await response.json();
-        this.render(aulas);
-      } catch (error) {
-        console.error('Erro ao carregar os dados das aulas:', error);
-      }
+        try {
+            const response = await fetch('aulas.json');
+            const aulas = await response.json();
+            this.render(aulas);
+        } catch (error) {
+            console.error('Erro ao carregar os dados das aulas:', error);
+        }
     }
-  
+
     render(aulas) {
-      const aulasDia = aulas.filter(a => a.data === this.hoje);
-  
-      const link = document.createElement('link');
-      link.rel = 'stylesheet';
-      link.href = 'styles_componente.css'; 
-      this.shadowRoot.appendChild(link); 
-  
-      this.shadowRoot.innerHTML += `
-        <div>
-          ${aulasDia.map(a => {
+        const aulasDia = aulas.filter(a => a.data === this.hoje);
+
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = 'styles_componente.css';
+        this.shadowRoot.appendChild(link);
+
+        this.shadowRoot.innerHTML += `
+          <div>
+            ${aulasDia.map(a => {
             let provaDisplay = a.prova_alert ? '' : 'display: none;';
+            let notaColor;
+            if (a.nota < 6) {
+                notaColor = 'red';
+            } else if (a.nota < 8) {
+                notaColor = 'orange';
+            } else {
+                notaColor = 'green';
+            }
             return `
-              <div class="comp-aula">
-                <div class="lable-prova p_lable" style="${provaDisplay}">PROVA: <b>${a.prova}</b></div>
-                <div class="titulo_aula">${a.disciplina}</div>
-                <p class="p">Local e Horário: <b>${a.local} - ${a.horario}</b></p>
-                <div class="lables">
-                  <div class="lable-frequencia p_lable">FALTAS: <b>${a.frequencia}</b></div>
-                  <div class="lable-nota p_lable">CR: <b>${a.nota}</b></div>
+                <div class="comp-aula">
+                  <div class="lable-prova p_lable" style="${provaDisplay}">PROVA: <b>${a.prova}</b></div>
+                  <div class="titulo_aula">${a.disciplina}</div>
+                  <p class="p">Local e Horário: <b>${a.local} - ${a.horario}</b></p>
+                  <div class="lables">
+                    <div class="lable-frequencia p_lable">FALTAS: <b>${a.frequencia}</b></div>
+                    <div class="lable-nota p_lable" style="color: ${notaColor};">CR: <b>${a.nota}</b></div>
+                  </div>
                 </div>
-              </div>
-            `;
-          }).join('')}
-        </div>
-      `;
+              `;
+        }).join('')}
+          </div>
+        `;
     }
-  }
-  
-  customElements.define('aulas-component', AulasComponent);  
+
+}
+
+customElements.define('aulas-component', AulasComponent);  
